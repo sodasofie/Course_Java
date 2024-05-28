410a380638a5afb0acda4b22674f947fb0ef6261
Sofie Pon <sonia10367@gmail.com>
2024-05-29 00:20:16 +0300

    Third test commit

--- src/main/java/com/example/fitness/Application.java
+++ src/main/java/com/example/fitness/Application.java
@@ -1,5 +1,6 @@
 package com.example.fitness;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -10,8 +11,7 @@ public class Application {
 
 		SpringApplication.run(Application.class, args);
 
-		System.out.println("help");
-
 	}
 
 }
+
