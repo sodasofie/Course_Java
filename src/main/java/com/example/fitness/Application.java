b220028da575f53c9da4994f6ffee21eec7ca4ee
Sofie Pon <sonia10367@gmail.com>
2024-05-28 23:32:00 +0300

    Second test

--- src/main/java/com/example/fitness/Application.java
+++ src/main/java/com/example/fitness/Application.java
@@ -7,7 +7,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(Application.class, args);
+
+		System.out.println("help");
+
 	}
 
 }
